7e55a17527aaba2d0b5544645c838c4760d02757
24071A05Q1 <mhruday481@gmail.com>
2026-04-21 00:30:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// In-memory database (array)
+let students = [];
+
+/* ---------------- CREATE ---------------- */
+app.post("/students", (req, res) => {
+    const student = req.body;
+    students.push(student);
+    res.send("Student added successfully");
+});
+app.get("/", (req, res) => {
+    res.send("Student CRUD API is running");
+});
+/* ---------------- READ ALL ---------------- */
+app.get("/students", (req, res) => {
+    res.json(students);
+});
+
+/* ---------------- READ ONE ---------------- */
+app.get("/students/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(students[id]);
+});
+
+/* ---------------- UPDATE ---------------- */
+app.put("/students/:id", (req, res) => {
+    const id = req.params.id;
+    students[id] = req.body;
+    res.send("Student updated successfully");
+});
+
+/* ---------------- DELETE ---------------- */
+app.delete("/students/:id", (req, res) => {
+    const id = req.params.id;
+    students.splice(id, 1);
+    res.send("Student deleted successfully");
+});
+
+/* ---------------- SERVER ---------------- */
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
