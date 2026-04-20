20c98ad7b7f30d25dfab5631ff9d03806daceba7
24071A05Q1 <mhruday481@gmail.com>
2026-04-21 00:42:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,3 +53,52 @@ app.delete("/students/:id", (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
+
+
+
+
+
+
+/*STEP 5: Test Using Postman
+🔹 1. Add Student (CREATE)
+
+Method: POST
+URL:
+
+http://localhost:3000/students
+
+Body → raw → JSON
+
+{
+  "name": "Hruday",
+  "age": 20,
+  "course": "CSE"
+}
+🔹 2. Get All Students (READ)
+
+Method: GET
+
+http://localhost:3000/students
+🔹 3. Get Student by ID
+
+Method: GET
+
+http://localhost:3000/students/0
+🔹 4. Update Student
+
+Method: PUT
+
+http://localhost:3000/students/0
+
+Body:
+
+{
+  "name": "Updated Name",
+  "age": 21,
+  "course": "IT"
+}
+🔹 5. Delete Student
+
+Method: DELETE
+
+http://localhost:3000/students/0*/
